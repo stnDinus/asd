0d83875ecde6d9b656df01a4582850c297de5237
stnDinus <111202214433@mhs.dinus.ac.id>
2025-04-16 16:50:03 +0700

    feat: LinkedList class

--- p6.cpp
+++ p6.cpp
@@ -10,81 +10,84 @@ struct Node {
   Node* next;
 };
 
-Node* HEAD = NULL;
+struct LinkedList {
+  Node* HEAD = NULL;
 
-void insert_depan(Node* temp, int x) {
-  temp->data = x;
+  void insert_depan(Node* temp, int x) {
+    temp->data = x;
 
-  if (HEAD != NULL) {
-    temp->next = HEAD;
-  }
-
-  HEAD = temp;
-}
-
-void insert_belakang(Node* temp, int x) {
-  temp->data = x;
-
-  Node* tail;
-  for (Node* i = HEAD; i != NULL; i = i->next) {
-    tail = i;
-  }
-
-  tail->next = temp;
-}
-
-void cetak() {
-  if (HEAD == NULL) {
-    cout << "-" << endl;
-    return;
-  }
-
-  for (Node* i = HEAD; i != NULL; i = i->next) {
-    cout << i->data;
-
-    if (i->next != NULL) {
-      cout << "->";
+    if (HEAD != NULL) {
+      temp->next = HEAD;
     }
+
+    HEAD = temp;
   }
 
-  cout << endl;
-}
+  void insert_belakang(Node* temp, int x) {
+    temp->data = x;
 
-void hapus_depan(Node* temp, int x) {
-  if (HEAD != NULL) {
-    HEAD = HEAD->next;
-  };
-}
-
-void hapus_belakang(Node* temp, int x) {
-  if (HEAD == NULL) {
-    return;
-  }
-  Node* pre_temp;
-  for (Node* i = HEAD; i != NULL ; i = i->next) {
-    if (i->next == NULL) {
-      break;
+    Node* tail;
+    for (Node* i = HEAD; i != NULL; i = i->next) {
+      tail = i;
     }
-    pre_temp = i;
+
+    tail->next = temp;
   }
-  pre_temp->next = NULL;
-}
+
+  void cetak() {
+    if (HEAD == NULL) {
+      cout << "-" << endl;
+      return;
+    }
+
+    for (Node* i = HEAD; i != NULL; i = i->next) {
+      cout << i->data;
+
+      if (i->next != NULL) {
+        cout << "->";
+      }
+    }
+
+    cout << endl;
+  }
+
+  void hapus_depan(Node* temp, int x) {
+    if (HEAD != NULL) {
+      HEAD = HEAD->next;
+    };
+  }
+
+  void hapus_belakang(Node* temp, int x) {
+    if (HEAD == NULL) {
+      return;
+    }
+    Node* pre_temp;
+    for (Node* i = HEAD; i != NULL ; i = i->next) {
+      if (i->next == NULL) {
+        break;
+      }
+      pre_temp = i;
+    }
+    pre_temp->next = NULL;
+  }
+};
 
 int main() {
-  insert_depan(new Node(), 1);
-  insert_depan(new Node(), 2);
+  LinkedList l;
+  l.insert_depan(new Node(), 1);
+  l.insert_depan(new Node(), 2);
 
-  insert_belakang(new Node(), 3);
+  l.insert_belakang(new Node(), 3);
 
-  cetak();
+  l.cetak();
 
-  hapus_depan(new Node(), 0);
+  l.hapus_depan(new Node(), 0);
 
-  cetak();
+  l.cetak();
 
-  hapus_belakang(new Node(), 0);
+  l.hapus_belakang(new Node(), 0);
 
-  cetak();
+  l.cetak();
 
   return 0;
 }
