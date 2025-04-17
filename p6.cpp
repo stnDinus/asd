fb29998c1a33485ef8ccda5745e11765f460110c
stnDinus <111202214433@mhs.dinus.ac.id>
2025-04-17 11:59:45 +0700

    feat: LinkedList insert_at method

--- p6.cpp
+++ p6.cpp
@@ -78,12 +78,40 @@ struct LinkedList {
     for (Node* n = HEAD; n != NULL; n = n->next, i++) {};
     return i;
   }
+
+  void insert_at(int at, int x) {
+    int l = len();
+    if (at > l - 1)  {
+      cerr << "invalid position " << at << " on linked list with length " << l << endl;
+      return;
+    }
+
+    if (at == 0) {
+      insert_head(x);
+    }
+
+    Node* temp = new Node();
+    temp->data = x;
+
+    int i = 0;
+    Node* pre_at;
+    for (Node* n = HEAD->next; n != NULL; n = n->next, i++) {
+      if (i == at) {
+        pre_at->next = temp;
+        temp->next = n;
+        break;
+      }
+      pre_at = n;
+    }
+  }
 };
 
 int main() {
   LinkedList l;
   l.insert_head(1);
   l.insert_head(2);
+  l.insert_head(4);
+  l.insert_head(7);
 
   l.insert_tail(3);
 
@@ -94,6 +122,18 @@ int main() {
   l.print();
 
   l.pop_back();
+
+  l.print();
+
+  l.insert_at(0, 9);
+
+  l.print();
+
+  l.insert_at(2, 8);
+
+  l.print();
+
+  l.insert_at(5, 0);
 
   l.print();
 
