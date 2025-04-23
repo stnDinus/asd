b30b542bdc7210e06f8284ded397b0eb5194f4a4
stnDinus <111202214433@mhs.dinus.ac.id>
2025-04-23 16:19:31 +0700

    perf: faster bubble sort

--- lib/bubble_sort.cpp
+++ lib/bubble_sort.cpp
@@ -1,9 +1,12 @@
 #include "lib.hpp"
 
 void bubble_sort(int a[], int n) {
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = 0; j < n - i - 1; j++) {
+  bool sorted = false;
+  while (!sorted) {
+    sorted = true;
+    for (int j = 0; j < n - 1; j++) {
       if (a[j] > a[j + 1]) {
+        sorted = false;
         swap(&a[j], &(a[j + 1]));
       }
     }
