681d548c670ba36b6b7bf1fef67e2bfe09571f8a
stnDinus <111202214433@mhs.dinus.ac.id>
2025-04-23 17:20:15 +0700

    fix: remove unused tmp

--- lib/selection_sort.cpp
+++ lib/selection_sort.cpp
@@ -1,7 +1,6 @@
 #include "lib.hpp"
 
-void selection_sort(int a [], int n) {
-  int tmp;
+void selection_sort(int a[], int n) {
   for (int i = 0; i < n; i++) {
     int smallest_idx = i;
     for (int j = i; j < n; j++) {
